7385efc6169ccb34c19c3521c73dffae18bbecf2
Daniele <daniele.latocca@gmail.com>
2016-02-08 17:57:14 +0100

    Initial commit

---
+++ module/Application/src/Application/Controller/ClienteController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class ClienteController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        $viewModel = new ViewModel();
+        return $viewModel;
+    }
+}
